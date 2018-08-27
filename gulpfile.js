cb690ecb9e966c69bc0e61381f293694993becd9
Aleksandr Kalinin <dev.lawejr@gmail.com>
2018-08-27 11:19:08 +0500

    implement gulp styles task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const env = process.env.NODE_ENV;
 const isDevelopment = !env || env === 'development';
 
+const path = require('path');
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')({
   rename: {
@@ -19,7 +20,8 @@ const paths = {
       basePath + 'views/pages/*.{html,njk}'
     ],
     styles: [
-      basePath + 'navigation.less'
+      basePath + 'styles/general.css',
+      basePath + 'styles/pages/*.css'
     ],
     scripts: {
       all: basePath + 'components/**/*.js',
@@ -28,26 +30,48 @@ const paths = {
   build: './build/',
 };
 
+const out = gulp.dest.bind(gulp, paths.build);
+
 gulp.task('templates', function () {
   console.log('========== Подготовка исходного HTML');
 
-  return gulp.src(paths.src.templates[0], { since: gulp.lastRun('templates') })
-    .pipe($.nunjucks({
-      path: paths.src.templates
+  return gulp
+    .src(paths.src.templates, { since: gulp.lastRun('templates') })
+    .pipe($.plumber({
+      errorHandler: $.notify.onError()
     }))
-    .pipe(gulp.dest(paths.build))
+    .pipe($.nunjucks({
+      data: {
+        Include: path.join(__dirname, basePath + 'views/_include/')
+      }
+    }))
+    .pipe(out())
     .pipe($.if(isDevelopment, browserSync.stream()))
 });
 
-gulp.task('build', gulp.parallel(
-  'templates',
-  // 'styles',
-  // 'scripts'
-));
+gulp.task('styles', function () {
+  console.log('========== Подготовка исходного CSS');
+
+  return gulp
+    .src(paths.src.styles, { since: gulp.lastRun('styles') })
+    .pipe($.plumber({
+      errorHandler: $.notify.onError()
+    }))
+    .pipe($.if(isDevelopment, $.stylelint({
+      reporters: [{ formatter: 'string', console: true }]
+    })))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 11'],
+      cascade: true
+    }),)
+    .pipe($.csso())
+    .pipe(out())
+    .pipe($.if(isDevelopment, browserSync.stream()))
+});
 
 gulp.task('watch', function () {
   gulp.watch(paths.src.templates, gulp.series('templates'));
-  // gulp.watch(paths.src.styles, gulp.series('styles:demo'));
+  gulp.watch(paths.src.styles, gulp.series('styles'));
   // $.if(isDevelopment, gulp.watch(paths.src.scripts.all, gulp.series('lint:js')));
 });
 
@@ -62,5 +86,14 @@ gulp.task('clean', function () {
 
   return del(paths.build)
 });
+
+gulp.task('build', gulp.series(
+  'clean',
+  gulp.parallel(
+    'templates',
+    'styles',
+    // 'scripts'
+  ))
+);
 
 gulp.task('default', gulp.series('build', gulp.parallel('watch', 'serve')));
